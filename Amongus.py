86e7bad9c2f13a70c8fdbde5e825102a35965da4
lordLegal <beneder.martin@gmail.com>
2020-10-24 11:34:40 +0200

    Update Amongus.py

--- Amongus.py
+++ Amongus.py
@@ -3,6 +3,7 @@ from discord import Member
 import random
 from discord.ext import commands, tasks
 import time
+import os
 
 client = commands.Bot(command_prefix = '.')
 client.remove_command('help')
@@ -16,4 +17,4 @@ async def code(ctx, code):
     await ctx.channel.purge(limit=1)
 
 
-client.run("NzYyMDE5MDkzMzI2NTk0MDc4.X3jDlQ.Ssjo9lzTfR9p2MuWpsAwHfzDq70")
+client.run(os.environ['DISCORD_TOKEN'])
