804f4f33d9c5cd35c2ca378d36566f3519f89982
lordLegal <beneder.martin@gmail.com>
2020-10-24 18:09:20 +0200

    Update Amongus.py

--- Amongus.py
+++ Amongus.py
@@ -11,6 +11,7 @@ client.remove_command('help')
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('Skalave von  .help '))
+    print("Bot is ready")
 
 @client.command()
 async def code(ctx, code):
