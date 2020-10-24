adcb3f87c4ead5b53260eb5d94dc10c5308833e8
lordLegal <beneder.martin@gmail.com>
2020-10-24 11:13:35 +0200

    Create Amongus.py

---
+++ Amongus.py
@@ -0,0 +1,19 @@
+import discord
+from discord import Member
+import random
+from discord.ext import commands, tasks
+import time
+
+client = commands.Bot(command_prefix = '.')
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Skalave von Autismus .help '))
+
+@client.command()
+async def code(ctx, code):
+    await ctx.channel.purge(limit=1)
+
+
+client.run("NzYyMDE5MDkzMzI2NTk0MDc4.X3jDlQ.95fkoiBUCwC_ygT5bxUy9SdG4ao")
